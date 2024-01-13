9797a15a39e35505f0868ff1268384a64721df32
Administratorius <lukaskaralius13@gmail.com>
2024-01-13 16:01:17 +0200

    update

--- runelite-client/src/main/java/net/runelite/client/plugins/firemaker/firemaker.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/firemaker/firemaker.java
@@ -1,27 +1,32 @@
 package net.runelite.client.plugins.firemaker;
 
+import com.google.inject.Provides;
 import net.runelite.api.Client;
+import net.runelite.client.config.ConfigManager;
 import net.runelite.client.eventbus.EventBus;
-import net.runelite.client.plugins.firemaker.tasks.LightFire;
+import net.runelite.client.plugins.firemaker.tasks.*;
 import net.unethicalite.api.plugins.Script;
-import net.runelite.client.plugins.firemaker.tasks.Bank;
-import net.runelite.client.plugins.firemaker.tasks.ScriptTask;
 import net.runelite.client.plugins.PluginDescriptor;
 import org.pf4j.Extension;
 
 import javax.inject.Inject;
+
+
+
 
 // This annotation is required in order for the client to detect it as a plugin/script.
 @PluginDescriptor(name = "Kuki firemaker", enabledByDefault = false)
 @Extension
 public class firemaker extends Script
 
+
 {
 	private static final ScriptTask[] TASKS = new ScriptTask[]{
-			new Bank(),
-			new LightFire()
+			new Banking(),
+			new LightFire(),
 	};
 
+	private boolean isRunning = false;
 	private boolean shouldLight = false;
 
 	@Inject
@@ -29,19 +34,33 @@ public class firemaker extends Script
 
 	@Inject
 	private EventBus eventBus;
+	@Inject
+	private firemakerConfig config;
 
+	@Provides
+	private firemakerConfig provideConfig(ConfigManager configManager)
+	{
+		return configManager.getConfig(firemakerConfig.class);
+	}
 
 	@Override
 	public void onStart(String... args)
 	{
+		isRunning = true;
 	}
 
-
+	@Override
+	protected void shutDown()
+	{
+	}
 
 
 	@Override
 	protected int loop()
 	{
+		if (!isRunning) {
+			return 0;
+		}
 		// Here I use task-based logic. You can also just write the entire script logic
 		for (ScriptTask task : TASKS)
 		{
